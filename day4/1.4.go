c9d624cf4e0ddf2d8d3a58a2d53ad4bc358994c3
chouthain <940249931@qq.com>
2019-05-13 19:07:52 +0800

    修正1.4.go

--- day4/1.4.go
+++ day4/1.4.go
@@ -2,9 +2,9 @@
 package main
  
 import (
-	bufio
-	fmt
-	os
+	"bufio"
+	"fmt"
+	"os"
 )
  
 type LnFile struct {
@@ -13,32 +13,32 @@ type LnFile struct {
 }
  
 func main() {
-	counts = make(map[string]LnFile)
-	files = os.Args[1]
+	counts := make(map[string]*LnFile)
+	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
 	} else {
-		for _, arg = range files {
-			f, err = os.Open(arg)
+		for _, arg := range files {
+			f, err := os.Open(arg)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, dup2%vn, err)
+				fmt.Fprintf(os.Stderr, "dup2:%v\n", err)
 			}
 			countLines(f, counts)
 			f.Close()
 		}
 	}
-	for line, n = range counts {
-		if n.Count  1 {
-			fmt.Printf(%d %vn%sn, n.Count, n.Filenames, line)
+	for line, n := range counts {
+		if n.Count > 1 {
+			fmt.Printf("%d %v\n%s\n", n.Count, n.Filenames, line)
 		}
 	}
 }
  
-func countLines(f os.File, counts map[string]LnFile) {
-	input = bufio.NewScanner(f)
+func countLines(f *os.File, counts map[string]*LnFile) {
+	input := bufio.NewScanner(f)
 	for input.Scan() {
-		key = input.Text()
-		_, ok = counts[key]
+		key := input.Text()
+		_, ok := counts[key]
 		if ok {
 			counts[key].Count++
 			counts[key].Filenames = append(counts[key].Filenames, f.Name())
@@ -49,8 +49,3 @@ func countLines(f os.File, counts map[string]LnFile) {
 		}
 	}
 }
---------------------- 
-作者：金戋 
-来源：CSDN 
-原文：httpsblog.csdn.netq1576962841articledetails85162346 
-版权声明：本文为博主原创文章，转载请附上博文链接！s
