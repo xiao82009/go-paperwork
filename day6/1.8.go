95a41a0c3f18da0800e54aac39ec252bbfb20837
chouthain <940249931@qq.com>
2019-05-14 16:04:36 +0800

    1.8和1.9的正经答案

---
+++ day6/1.8.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = strings.Join([]string{"http://", url}, "")
+		}
+		fmt.Printf("%s\n", url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", b)
+	}
+}
